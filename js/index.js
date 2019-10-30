2553aa3a64e302c56beb705efcb55d43a0b0029a
WangShuoQ <2898463148@qq.com>
2019-10-31 00:03:52 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function(){
+	// 为栏目绑定事件
+	$("ul.left_nav").on("click","li",function(){
+		// this = event.currentTarget]
+		$("ul.left_nav > li").removeClass("current");
+		$(this).addClass("current");
+		var url = $(this).attr("url");
+		// 拼接时间戳的目的是为了消除缓存
+		$("#wrapper").load(url+"?time="+Math.random())
+	})
+
+	// 默认选中第一个li
+	$("ul.left_nav > li:first-child").trigger("click");
+});
+      	//刷新
+	function newhome(){
+    window.self.location.reload();
+}
